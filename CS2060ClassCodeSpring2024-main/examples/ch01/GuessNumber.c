8d325592cc7dee9c4e34ae14d29d23f266e12b91
kdennebr <kayden9stma@gmail.com>
2024-01-21 18:43:24 -0700

    Add Comments

--- CS2060ClassCodeSpring2024-main/examples/ch01/GuessNumber.c
+++ CS2060ClassCodeSpring2024-main/examples/ch01/GuessNumber.c
@@ -1,59 +1,59 @@
 // Randomly generate numbers between a min and max for user to guess.
 
 
-//
+//adding in all the extras
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-//
+//creating variables for min and max
 #define MIN 1
 #define MAX 1000
 
-//
-void guessGame(void); 
+//initializing functions
+void guessGame(void);
 bool isCorrect(int guess, int answer); 
 
 int main(void) {
-    //
+    //random number based on current time
    srand(time(0)); 
 
-   //
+   //runs guessGame function
    guessGame();
 } // end main
 
 
 
-// 
+//guessGame function, returns nothing and takes nothing
 void guessGame(void) {
     
-    //
+    //initialize variables
    int response =0;
    int guess = 0;
 
     printf("Hello Deb");
-   //
+   //do while loop
    do {
 
-      // 
+      //Creates answer variables that is 1 + a random val
       int answer = 1 + rand() % MAX;
 
-      //
+      //Displays rules to user
       printf("I have a number between %d and %d.\n", MIN, MAX);
 
-      // 
-      puts("Can you guess my number?\n" 
+      // Prompts user
+      puts("Can you guess my number?\n"
            "Please type your first guess.");
 
-      //
+      //Adds question mark
       printf("%s", "? ");
       
 
-      //
+      //takes user input in
       scanf("%d", &guess);
 
-      // 
+      // while user input doesnt equal answer using isCorrect function
       while (!isCorrect(guess, answer)) {
          scanf("%d", &guess);
       }
@@ -68,18 +68,18 @@ void guessGame(void) {
    } while (response == 1);
 } // end function guessGame
 
-// 
+// isCorrect function, returns boolean and takes two ints
 bool isCorrect(int guess, int answer) {
 
-    //
+    //initialize return as false
     bool correct = false;
 
-   // 
+   // change return to true if guess equals answer
    if (guess == answer) {
       correct = true;
    }
 
-   // 
+   // if guess is less than answer print Too Low, else print Too High
    if (guess < answer) {
       printf( "%s", "Too low. Try again.\n? " );
    }
