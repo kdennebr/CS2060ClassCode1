031dac6efcb90ce7475d0ef113b23d5ac16fb38f
kdennebr <kayden9stma@gmail.com>
2024-02-13 21:59:45 -0700

    Iteration01
    Iteration01

--- CS2060ClassCodeSpring2024-main/examples/ch03/fig03_06.c
+++ CS2060ClassCodeSpring2024-main/examples/ch03/fig03_06.c
@@ -7,25 +7,33 @@ int main(void) {
    // initialize variables in definitions 
    int passes = 0; 
    int failures = 0; 
-   int student = 1;  
+   int student = 1; 
+    int scanfReturn = 0;
 
    // process 10 students using counter-controlled loop 
    while (student <= 10) {
       // prompt user for input and obtain value from user 
       printf("%s", "Enter result (1=pass,2=fail): ");
       int result = 0; // one exam result 
-      scanf("%d", &result);
+      scanfReturn = scanf("%d", &result);
 
-      // if result 1, increment passes 
-      if (result == 1) {     
-         passes = passes + 1;
-      } // end if 
-      else { // otherwise, increment failures 
-         failures = failures + 1;
-      } // end else 
-
-      student = student + 1; // increment student counter  
-   } // end while 
+       if(scanfReturn == 1 && (result == 1 || result == 2)){
+           // if result 1, increment passes
+           if (result == 1) {
+               passes = passes + 1;
+           } // end if
+           else { // otherwise, increment failures
+               failures = failures + 1;
+           } // end else
+           
+           
+           
+           student = student + 1;
+       }
+       else{
+           puts("Try again");
+       }// increment student counter
+   } // end while
 
    // termination phase; display number of passes and failures 
    printf("Passed %d\n", passes);
--- CS2060ClassCodeSpring2024-main/examples/ch04/fig04_05.c
+++ CS2060ClassCodeSpring2024-main/examples/ch04/fig04_05.c
@@ -1,6 +1,6 @@
 // fig04_05.c
 // Counting letter grades with switch.
-//Ver 1
+//Ver 2
 #include <stdio.h>
 
 int main(void) {
@@ -14,40 +14,41 @@ int main(void) {
    puts("Enter the EOF character to end input.");
    int grade = 0; // one grade 
 
-   // loop until user types end-of-file key sequence
+    // loop until user types end-of-file key sequence
    while ((grade = getchar()) != EOF) {
 
-      // determine which grade was input
-      switch (grade) { // switch nested in while
-         case 'A': // grade was uppercase A
-         case 'a': // or lowercase a
-            ++aCount; 
-            break; // necessary to exit switch
-         case 'B': // grade was uppercase B
-         case 'b': // or lowercase b
-            ++bCount; 
-            break; 
-         case 'C': // grade was uppercase C
-         case 'c': // or lowercase c
-            ++cCount; 
-            break;
-         case 'D': // grade was uppercase D
-         case 'd': // or lowercase d
-            ++dCount; 
-            break;
-         case 'F': // grade was uppercase F
-         case 'f': // or lowercase f
-            ++fCount; 
-            break; 
-         case '\n': // ignore newlines,
-         case '\t': // tabs,
-         case ' ': // and spaces in input
-            break; 
-         default: // catch all other characters
-            printf("%s", "Incorrect letter grade entered."); 
-            puts(" Enter a new grade."); 
-            break; // optional; will exit switch anyway
-      } // end switch 
+           // determine which grade was input
+           switch (grade) { // switch nested in while
+               case 'A': // grade was uppercase A
+               case 'a': // or lowercase a
+                   ++aCount;
+                   break; // necessary to exit switch
+               case 'B': // grade was uppercase B
+               case 'b': // or lowercase b
+                   ++bCount;
+                   break;
+               case 'C': // grade was uppercase C
+               case 'c': // or lowercase c
+                   ++cCount;
+                   break;
+               case 'D': // grade was uppercase D
+               case 'd': // or lowercase d
+                   ++dCount;
+                   break;
+               case 'F': // grade was uppercase F
+               case 'f': // or lowercase f
+                   ++fCount;
+                   break;
+               case '\n': // ignore newlines,
+               case '\t': // tabs,
+               case ' ': // and spaces in input
+                   break;
+               default: // catch all other characters
+                   printf("%s", "Incorrect letter grade entered.");
+                   puts(" Enter a new grade.");
+                   break; // optional; will exit switch anyway
+           } // end switch
+       
    } // end while
 
    // output summary of results
